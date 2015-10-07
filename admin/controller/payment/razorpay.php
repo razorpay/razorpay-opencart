0d69d12db12705f70c4f835fff82031a46b0f6e4
Abhay Rana <nemo@razorpay.com>
2015-10-08 01:14:40 +0530

    Style fixes

--- admin/controller/payment/razorpay.php
+++ admin/controller/payment/razorpay.php
@@ -1,36 +1,39 @@
-<?php 
-class ControllerPaymentRazorpay extends Controller {
-    private $error = array(); 
+<?php
 
-    public function index() {
+class ControllerPaymentRazorpay extends Controller
+{
+    private $error = array();
+
+    public function index()
+    {
         $this->language->load('payment/razorpay');
 
         $this->document->setTitle($this->language->get('heading_title'));
-        
+
         $this->load->model('setting/setting');
-            
+
         if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-            $this->model_setting_setting->editSetting('razorpay', $this->request->post);                
-            
-            $this->db->query("DELETE FROM " . DB_PREFIX . "url_alias WHERE query = 'callback=1' AND keyword = 'payment-callback'");
-            $this->db->query("INSERT INTO " . DB_PREFIX . "url_alias SET query = 'callback=1', keyword = 'payment-callback'");
+            $this->model_setting_setting->editSetting('razorpay', $this->request->post);
+
+            $this->db->query('DELETE FROM '.DB_PREFIX."url_alias WHERE query = 'callback=1' AND keyword = 'payment-callback'");
+            $this->db->query('INSERT INTO '.DB_PREFIX."url_alias SET query = 'callback=1', keyword = 'payment-callback'");
 
             $this->session->data['success'] = $this->language->get('text_success');
 
-            $this->redirect($this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'));
+            $this->redirect($this->url->link('extension/payment', 'token='.$this->session->data['token'], 'SSL'));
         }
 
         $this->data['heading_title'] = $this->language->get('heading_title');
 
         $this->data['text_enabled'] = $this->language->get('text_enabled');
         $this->data['text_disabled'] = $this->language->get('text_disabled');
-        
+
         $this->data['entry_key_id'] = $this->language->get('entry_key_id');
         $this->data['entry_key_secret'] = $this->language->get('entry_key_secret');
-        $this->data['entry_order_status'] = $this->language->get('entry_order_status');       
+        $this->data['entry_order_status'] = $this->language->get('entry_order_status');
         $this->data['entry_status'] = $this->language->get('entry_status');
         $this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
-        
+
         $this->data['button_save'] = $this->language->get('button_save');
         $this->data['button_cancel'] = $this->language->get('button_cancel');
 
@@ -51,31 +54,31 @@ class ControllerPaymentRazorpay extends Controller {
         } else {
             $this->data['error_key_secret'] = '';
         }
-        
+
         $this->data['breadcrumbs'] = array();
 
         $this->data['breadcrumbs'][] = array(
-            'text'      => $this->language->get('text_home'),
-            'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
-            'separator' => false
+            'text' => $this->language->get('text_home'),
+            'href' => $this->url->link('common/home', 'token='.$this->session->data['token'], 'SSL'),
+            'separator' => false,
         );
 
         $this->data['breadcrumbs'][] = array(
-            'text'      => $this->language->get('text_payment'),
-            'href'      => $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'),
-            'separator' => ' :: '
+            'text' => $this->language->get('text_payment'),
+            'href' => $this->url->link('extension/payment', 'token='.$this->session->data['token'], 'SSL'),
+            'separator' => ' :: ',
         );
 
         $this->data['breadcrumbs'][] = array(
-            'text'      => $this->language->get('heading_title'),
-            'href'      => $this->url->link('payment/razorpay', 'token=' . $this->session->data['token'], 'SSL'),
-            'separator' => ' :: '
+            'text' => $this->language->get('heading_title'),
+            'href' => $this->url->link('payment/razorpay', 'token='.$this->session->data['token'], 'SSL'),
+            'separator' => ' :: ',
         );
-                
-        $this->data['action'] = $this->url->link('payment/razorpay', 'token=' . $this->session->data['token'], 'SSL');
-        
-        $this->data['cancel'] = $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL');
-    
+
+        $this->data['action'] = $this->url->link('payment/razorpay', 'token='.$this->session->data['token'], 'SSL');
+
+        $this->data['cancel'] = $this->url->link('extension/payment', 'token='.$this->session->data['token'], 'SSL');
+
         if (isset($this->request->post['razorpay_key_id'])) {
             $this->data['razorpay_key_id'] = $this->request->post['razorpay_key_id'];
         } else {
@@ -91,20 +94,19 @@ class ControllerPaymentRazorpay extends Controller {
         if (isset($this->request->post['razorpay_order_status_id'])) {
             $this->data['razorpay_order_status_id'] = $this->request->post['razorpay_order_status_id'];
         } else {
-            $this->data['razorpay_order_status_id'] = $this->config->get('razorpay_order_status_id'); 
-        } 
+            $this->data['razorpay_order_status_id'] = $this->config->get('razorpay_order_status_id');
+        }
 
         $this->load->model('localisation/order_status');
-        
+
         $this->data['order_statuses'] = $this->model_localisation_order_status->getOrderStatuses();
-        
-        
+
         if (isset($this->request->post['razorpay_status'])) {
             $this->data['razorpay_status'] = $this->request->post['razorpay_status'];
         } else {
             $this->data['razorpay_status'] = $this->config->get('razorpay_status');
         }
-        
+
         if (isset($this->request->post['razorpay_sort_order'])) {
             $this->data['razorpay_sort_order'] = $this->request->post['razorpay_sort_order'];
         } else {
@@ -114,17 +116,18 @@ class ControllerPaymentRazorpay extends Controller {
         $this->template = 'payment/razorpay.tpl';
         $this->children = array(
             'common/header',
-            'common/footer'
+            'common/footer',
         );
-                
+
         $this->response->setOutput($this->render());
     }
 
-    protected function validate() {
+    protected function validate()
+    {
         if (!$this->user->hasPermission('modify', 'payment/razorpay')) {
             $this->error['warning'] = $this->language->get('error_permission');
         }
-        
+
         if (!$this->request->post['razorpay_key_id']) {
             $this->error['razorpay_key_id'] = $this->language->get('error_key_id');
         }
@@ -132,42 +135,38 @@ class ControllerPaymentRazorpay extends Controller {
         if (!$this->request->post['razorpay_key_secret']) {
             $this->error['razorpay_key_secret'] = $this->language->get('error_key_secret');
         }
-        
+
         if (!$this->error) {
             return true;
         } else {
             return false;
-        }   
+        }
     }
 
     private function is_serialized($value, &$result = null)
     {
         // Bit of a give away this one
-        if (!is_string($value))
-        {
+        if (!is_string($value)) {
             return false;
         }
-        if (empty($value))
-        {
+        if (empty($value)) {
             return false;
         }
         // Serialized false, return true. unserialize() returns false on an
         // invalid string or it could return false if the string is serialized
         // false, eliminate that possibility.
-        if ($value === 'b:0;')
-        {
+        if ($value === 'b:0;') {
             $result = false;
+
             return true;
         }
-     
+
         $length = strlen($value);
-        $end    = '';
-     
-        switch ($value[0])
-        {
+        $end = '';
+
+        switch ($value[0]) {
             case 's':
-                if ($value[$length - 2] !== '"')
-                {
+                if ($value[$length - 2] !== '"') {
                     return false;
                 }
             case 'b':
@@ -178,14 +177,12 @@ class ControllerPaymentRazorpay extends Controller {
             case 'a':
             case 'O':
                 $end .= '}';
-     
-                if ($value[1] !== ':')
-                {
+
+                if ($value[1] !== ':') {
                     return false;
                 }
-     
-                switch ($value[2])
-                {
+
+                switch ($value[2]) {
                     case 0:
                     case 1:
                     case 2:
@@ -197,29 +194,28 @@ class ControllerPaymentRazorpay extends Controller {
                     case 8:
                     case 9:
                     break;
-     
+
                     default:
                         return false;
                 }
             case 'N':
                 $end .= ';';
-     
-                if ($value[$length - 1] !== $end[0])
-                {
+
+                if ($value[$length - 1] !== $end[0]) {
                     return false;
                 }
             break;
-     
+
             default:
                 return false;
         }
-     
-        if (($result = @unserialize($value)) === false)
-        {
+
+        if (($result = @unserialize($value)) === false) {
             $result = null;
+
             return false;
         }
+
         return true;
     }
 }
-?>
--- catalog/controller/payment/razorpay.php
+++ catalog/controller/payment/razorpay.php
@@ -1,6 +1,9 @@
 <?php
-class ControllerPaymentRazorpay extends Controller {
-    protected function index() {
+
+class ControllerPaymentRazorpay extends Controller
+{
+    protected function index()
+    {
         $this->language->load('payment/razorpay');
 
         $this->data['button_confirm'] = $this->language->get('button_confirm');
@@ -8,24 +11,24 @@ class ControllerPaymentRazorpay extends Controller {
         $this->data['version'] = $this->language->get('version');
         $this->data['text_wait'] = $this->language->get('text_wait');
         $this->data['text_attention'] = $this->language->get('text_attention');
-        
+
         $this->load->model('checkout/order');
 
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
 
         $this->data['key_id'] = $this->config->get('razorpay_key_id');
         $this->data['currency_code'] = $order_info['currency_code'];
-        $this->data['total'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false)*100;
+        $this->data['total'] = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false) * 100;
         $this->data['merchant_order_id'] = $this->session->data['order_id'];
-        $this->data['card_holder_name'] = $order_info['payment_firstname'] . ' ' . $order_info['payment_lastname'];
+        $this->data['card_holder_name'] = $order_info['payment_firstname'].' '.$order_info['payment_lastname'];
         $this->data['email'] = $order_info['email'];
         $this->data['phone'] = $order_info['telephone'];
-        $this->data['name'] = $this->config->get("config_name");
+        $this->data['name'] = $this->config->get('config_name');
         $this->data['lang'] = $this->session->data['language'];
         $this->data['return_url'] = $this->url->link('payment/razorpay/callback', '', 'SSL');
-  
-        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/razorpay.tpl')) {
-            $this->template = $this->config->get('config_template') . '/template/payment/razorpay.tpl';
+
+        if (file_exists(DIR_TEMPLATE.$this->config->get('config_template').'/template/payment/razorpay.tpl')) {
+            $this->template = $this->config->get('config_template').'/template/payment/razorpay.tpl';
         } else {
             $this->template = 'default/template/payment/razorpay.tpl';
         }
@@ -33,139 +36,128 @@ class ControllerPaymentRazorpay extends Controller {
         $this->render();
     }
 
-    public function callback() {
-
+    public function callback()
+    {
         $request_params = array_merge($_GET, $_POST);
         $this->load->model('checkout/order');
-        if(isset($request_params['razorpay_payment_id']) and isset($request_params['merchant_order_id'])) {
+        if (isset($request_params['razorpay_payment_id']) and isset($request_params['merchant_order_id'])) {
             $razorpay_payment_id = $request_params['razorpay_payment_id'];
             $merchant_order_id = $request_params['merchant_order_id'];
             $key_id = $this->config->get('razorpay_key_id');
             $key_secret = $this->config->get('razorpay_key_secret');
-            
-            $order_info = $this->model_checkout_order->getOrder($merchant_order_id);       
-            $amount = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false)*100;
-            
+
+            $order_info = $this->model_checkout_order->getOrder($merchant_order_id);
+            $amount = $this->currency->format($order_info['total'], $order_info['currency_code'], $order_info['currency_value'], false) * 100;
+
             $success = false;
-            $error = "";
+            $error = '';
 
             try {
-                    $url = 'https://api.razorpay.com/v1/payments/'.$razorpay_payment_id.'/capture';
-                    $fields_string="amount=$amount";
+                $url = 'https://api.razorpay.com/v1/payments/'.$razorpay_payment_id.'/capture';
+                $fields_string = "amount=$amount";
 
                     //cURL Request
                     $ch = curl_init();
 
                     //set the url, number of POST vars, POST data
-                    curl_setopt($ch,CURLOPT_URL, $url);
-                    curl_setopt($ch,CURLOPT_USERPWD, $key_id . ":" . $key_secret);
-                    curl_setopt($ch,CURLOPT_TIMEOUT, 60);
-                    curl_setopt($ch,CURLOPT_POST, 1);
-                    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-                    curl_setopt($ch,CURLOPT_RETURNTRANSFER, TRUE);
+                    curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_USERPWD, $key_id.':'.$key_secret);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
                     //execute post
                     $result = curl_exec($ch);
-                    $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-
-                    if($result === false) {
-                        $success = false;
-                        $error = 'Curl error: ' . curl_error($ch);
-                    }
-                    else {
-                        $response_array = json_decode($result, true);
+                if ($result === false) {
+                    $success = false;
+                    $error = 'Curl error: '.curl_error($ch);
+                } else {
+                    $response_array = json_decode($result, true);
                         //Check success response
-                        if($http_status === 200 and isset($response_array['error']) === false){
-                            $success = true;    
-                        }
-                        else {
+                        if ($http_status === 200 and isset($response_array['error']) === false) {
+                            $success = true;
+                        } else {
                             $success = false;
 
-                            if(!empty($response_array['error']['code'])) {
-                                $error = $response_array['error']['code'].":".$response_array['error']['description'];
-                            }
-                            else {
-                                $error = "RAZORPAY_ERROR:Invalid Response <br/>".$result;
+                            if (!empty($response_array['error']['code'])) {
+                                $error = $response_array['error']['code'].':'.$response_array['error']['description'];
+                            } else {
+                                $error = 'RAZORPAY_ERROR:Invalid Response <br/>'.$result;
                             }
                         }
-                    }
-                        
+                }
+
                     //close connection
                     curl_close($ch);
-                }
-                catch (Exception $e) {
-                    $success = false;
-                    $error ="OPENCART_ERROR:Request to Razorpay Failed";
-                }
+            } catch (Exception $e) {
+                $success = false;
+                $error = 'OPENCART_ERROR:Request to Razorpay Failed';
+            }
 
             if ($success === true) {
                 if (!$order_info['order_status_id']) {
-                    $this->model_checkout_order->confirm($merchant_order_id, $this->config->get('razorpay_order_status_id'),'Payment Successful. Razorpay Payment Id:'.$razorpay_payment_id,true);
+                    $this->model_checkout_order->confirm($merchant_order_id, $this->config->get('razorpay_order_status_id'), 'Payment Successful. Razorpay Payment Id:'.$razorpay_payment_id, true);
                 } else {
-                    $this->model_checkout_order->update($merchant_order_id, $this->config->get('razorpay_order_status_id'),'Payment Successful. Razorpay Payment Id:'.$razorpay_payment_id,true);
+                    $this->model_checkout_order->update($merchant_order_id, $this->config->get('razorpay_order_status_id'), 'Payment Successful. Razorpay Payment Id:'.$razorpay_payment_id, true);
                 }
 
-                echo '<html>' . "\n";
-                echo '<head>' . "\n";
-                echo '  <meta http-equiv="Refresh" content="0; url=' . $this->url->link('checkout/success') . '">' . "\n";
-                echo '</head>'. "\n";
-                echo '<body>' . "\n";
-                echo 'Payment Successful. <p>Please <a href="' . $this->url->link('checkout/success') . '">click here to continue</a>!</p>' . "\n";
-                echo '</body>' . "\n";
-                echo '</html>' . "\n";
+                echo '<html>'."\n";
+                echo '<head>'."\n";
+                echo '  <meta http-equiv="Refresh" content="0; url='.$this->url->link('checkout/success').'">'."\n";
+                echo '</head>'."\n";
+                echo '<body>'."\n";
+                echo 'Payment Successful. <p>Please <a href="'.$this->url->link('checkout/success').'">click here to continue</a>!</p>'."\n";
+                echo '</body>'."\n";
+                echo '</html>'."\n";
                 exit();
-            }   
-            else {
+            } else {
                 if (!$order_info['order_status_id']) {
-                    $this->model_checkout_order->confirm($merchant_order_id, 10,$error.' Payment Failed! Check Razorpay dashboard for details of Payment Id:'.$razorpay_payment_id,true);
+                    $this->model_checkout_order->confirm($merchant_order_id, 10, $error.' Payment Failed! Check Razorpay dashboard for details of Payment Id:'.$razorpay_payment_id, true);
                 } else {
-                    $this->model_checkout_order->update($merchant_order_id, 10,$error.' Payment Failed! Check Razorpay dashboard for details of Payment Id:'.$razorpay_payment_id,true);
+                    $this->model_checkout_order->update($merchant_order_id, 10, $error.' Payment Failed! Check Razorpay dashboard for details of Payment Id:'.$razorpay_payment_id, true);
                 }
 
-
-                echo '<html>' . "\n";
-                echo '<head>' . "\n";
-                echo '</head>'. "\n";
-                echo '<body>' . "\n";
-                echo 'Payment Failed. <p>Please <a href="' . $this->url->link('checkout/checkout') . '">click here to continue</a>!</p>' . "\n";
-                echo '</body>' . "\n";
-                echo '</html>' . "\n";
+                echo '<html>'."\n";
+                echo '<head>'."\n";
+                echo '</head>'."\n";
+                echo '<body>'."\n";
+                echo 'Payment Failed. <p>Please <a href="'.$this->url->link('checkout/checkout').'">click here to continue</a>!</p>'."\n";
+                echo '</body>'."\n";
+                echo '</html>'."\n";
                 exit();
             }
         } else {
-            echo 'An error occured. Contact site administrator, please!'; 
-        }       
+            echo 'An error occured. Contact site administrator, please!';
+        }
     }
 
     private function is_serialized($value, &$result = null)
     {
         // Bit of a give away this one
-        if (!is_string($value))
-        {
+        if (!is_string($value)) {
             return false;
         }
-        if (empty($value))
-        {
+        if (empty($value)) {
             return false;
         }
         // Serialized false, return true. unserialize() returns false on an
         // invalid string or it could return false if the string is serialized
         // false, eliminate that possibility.
-        if ($value === 'b:0;')
-        {
+        if ($value === 'b:0;') {
             $result = false;
+
             return true;
         }
-     
+
         $length = strlen($value);
-        $end    = '';
-     
-        switch ($value[0])
-        {
+        $end = '';
+
+        switch ($value[0]) {
             case 's':
-                if ($value[$length - 2] !== '"')
-                {
+                if ($value[$length - 2] !== '"') {
                     return false;
                 }
             case 'b':
@@ -176,14 +168,12 @@ class ControllerPaymentRazorpay extends Controller {
             case 'a':
             case 'O':
                 $end .= '}';
-     
-                if ($value[1] !== ':')
-                {
+
+                if ($value[1] !== ':') {
                     return false;
                 }
-     
-                switch ($value[2])
-                {
+
+                switch ($value[2]) {
                     case 0:
                     case 1:
                     case 2:
@@ -195,30 +185,28 @@ class ControllerPaymentRazorpay extends Controller {
                     case 8:
                     case 9:
                     break;
-     
+
                     default:
                         return false;
                 }
             case 'N':
                 $end .= ';';
-     
-                if ($value[$length - 1] !== $end[0])
-                {
+
+                if ($value[$length - 1] !== $end[0]) {
                     return false;
                 }
             break;
-     
+
             default:
                 return false;
         }
-     
-        if (($result = @unserialize($value)) === false)
-        {
+
+        if (($result = @unserialize($value)) === false) {
             $result = null;
+
             return false;
         }
+
         return true;
     }
 }
-
-?>
