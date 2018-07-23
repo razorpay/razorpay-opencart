b08ec07d628b5173c38c2458292107cc0ce02473
Narendra <narendra.arora@evontech.com>
2018-07-23 19:48:48 +0530

    Opencart 3.X Update code to opencart 3.X standard

---
+++ admin/language/en-gb/extension/payment/razorpay.php
@@ -0,0 +1,26 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'Razorpay';
+
+// Text 
+$_['text_payment'] = 'Payment';
+$_['text_edit'] = 'Edit Razorpay';
+$_['text_success'] = 'Success: You have modified Razorpay account details!';
+$_['text_razorpay'] = '<a href="https://www.razorpay.com" target="_blank"><img src="view/image/payment/razorpay.png" alt="Razorpay" title="Razorpay" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Entry
+$_['entry_key_id'] = 'Razorpay Key Id';
+$_['entry_key_secret'] = 'Razorpay Key Secret';
+$_['entry_order_status'] = 'Order Status';
+$_['entry_status'] = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+//tooltips
+$_['help_key_id'] = 'The Api Key Id and Key Secret you will recieve from the API keys section of Razorpay Dashboard. Use test Key for testing purposes.';
+$_['help_order_status'] = 'The status of the order to be marked on completion of payment.';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify payment Razorpay!';
+$_['error_key_id'] = 'Key Id Required!';
+$_['error_key_secret'] = 'Key Secret Required!';
