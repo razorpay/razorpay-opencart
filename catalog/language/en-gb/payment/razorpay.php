bf23cb21421567f11b637ff39a39b1ef15d23945
nikhilsharma1100 <102040769+nikhilsharma1100@users.noreply.github.com>
2023-09-26 00:39:57 +0530

    updated language files to fix payment methods html tag

--- catalog/language/en-gb/payment/razorpay.php
+++ catalog/language/en-gb/payment/razorpay.php
@@ -2,7 +2,7 @@
 /* Razorpay extension constants */
 // Text
 $_['heading_title'] = 'Pay by Razorpay'; // Subscriptions
-$_['text_title'] = 'Pay by Razorpay <br> <a href="https://www.razorpay.com" target="_blank"><img src="https://cdn.razorpay.com/static/assets/logo/payment_method.svg" alt="Razorpay" title="Razorpay" style="border: 1px solid #EEEEEE;margin-top: 5px" /></a>';
+$_['text_title'] = 'Pay by Razorpay <br> <a href=\'https://www.razorpay.com\' target=\'_blank\'><img src=\'https://cdn.razorpay.com/static/assets/logo/payment_method.svg\' alt=\'Razorpay\' title=\'Razorpay\' style=\'border: 1px solid #EEEEEE;margin-top: 5px\' /></a>';
 
 // Subscription
 $_['text_account'] = 'Account';
