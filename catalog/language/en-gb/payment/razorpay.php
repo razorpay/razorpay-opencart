e1d5fbacc6897dee356fbb5a1f84c40cec9386c1
nikhilsharma1100 <102040769+nikhilsharma1100@users.noreply.github.com>
2023-09-26 22:54:18 +0530

    [QA] Updated payment methods constants

--- catalog/language/en-gb/payment/razorpay.php
+++ catalog/language/en-gb/payment/razorpay.php
@@ -1,8 +1,8 @@
 <?php
 /* Razorpay extension constants */
 // Text
-$_['heading_title'] = 'Pay by Razorpay'; // Subscriptions
-$_['text_title'] = 'Pay by Razorpay <br> <a href=\'https://www.razorpay.com\' target=\'_blank\'><img src=\'https://cdn.razorpay.com/static/assets/logo/payment_method.svg\' alt=\'Razorpay\' title=\'Razorpay\' style=\'border: 1px solid #EEEEEE;margin-top: 5px\' /></a>';
+$_['heading_title'] = 'Pay via Razorpay'; // Subscriptions
+$_['text_title'] = 'Razorpay (UPI, Cards, Wallets, Netbanking)';
 
 // Subscription
 $_['text_account'] = 'Account';
