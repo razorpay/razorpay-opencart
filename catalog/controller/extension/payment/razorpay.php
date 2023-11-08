e2d9f8ebeb5b6a7a92523a10780ca318cae72dd8
Razorpay <abdul.sharief@razorpay.com>
2023-11-08 10:09:36 +0530

    Blocked KWD|OMR|BHD currencies

--- catalog/controller/extension/payment/razorpay.php
+++ catalog/controller/extension/payment/razorpay.php
@@ -20,6 +20,11 @@ class ControllerExtensionPaymentRazorpay extends Controller
     const SUBSCRIPTION_CHARGED      = 'subscription.charged';
     const WEBHOOK_WAIT_TIME         = 30;
     const HTTP_CONFLICT_STATUS      = 409;
+    const CURRENCY_NOT_ALLOWED  = [
+        'KWD',
+        'OMR',
+        'BHD',
+    ];
 
     // Set RZP plugin version
     private $version = '5.1.1';
@@ -40,6 +45,13 @@ class ControllerExtensionPaymentRazorpay extends Controller
         $this->load->model('checkout/order');
 
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+
+        if (in_array($order_info['currency_code'],  self::CURRENCY_NOT_ALLOWED) === true)
+        {
+            $this->log->write("Order creation failed, because currency (" . $order_info['currency_code'] . ") not supported");
+            echo "<div class='alert alert-danger alert-dismissible'>Order creation failed, because currency (" . $order_info['currency_code'] . ") not supported.</div>";
+            exit ;
+        }
 
         try
         {
